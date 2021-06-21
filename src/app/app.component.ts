7865e7bf0b7accee796c0ba73f03ca5d6b1bc35d
Julian Pömp <j.poemp@campus.lmu.de>
2021-06-21 10:36:59 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MdbDropdownDirective} from 'mdb-angular-ui-kit/dropdown';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mdbTest';
+  @ViewChild('dropdown') dropdown: MdbDropdownDirective | undefined;
+
+  toggle() {
+    console.log(`clicked`);
+    if (this.dropdown !== undefined) {
+      console.log(this.dropdown);
+      this.dropdown.show();
+    }
+  }
 }
